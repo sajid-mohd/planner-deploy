0e462538845cbadc90a15e2f92b89207181204e5
Mohammad Sajid <159249332+sajid-mohd@users.noreply.github.com>
2026-06-10 01:06:23 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,16 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+    bcrypt__rounds=12
+)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
+    if isinstance(password, str):
+        password = password.encode("utf-8")
+    password = password[:72]
     return pwd_context.hash(password)
